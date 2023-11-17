901d5d16116038fbc827c53f7983ebca91907067
Christopher Bellanger <christopher.bellanger@teester.com>
2023-11-17 13:52:59 +0100

    remove prerender from auth callback

--- src/pages/auth/callback.ts
+++ src/pages/auth/callback.ts
@@ -1,6 +1,8 @@
 import { createServerClient } from '@supabase/ssr';
 import { type APIRoute } from 'astro';
 
+export const prerender = false;
+
 export const GET: APIRoute = async ({ request, cookies, redirect }) => {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
