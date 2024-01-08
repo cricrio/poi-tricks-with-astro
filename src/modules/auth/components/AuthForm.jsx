fc87593131ac67629cdf75559619320940dc5fb9
Christopher Bellanger <christopher.bellanger@teester.com>
2024-01-08 12:25:30 +0100

    improve login page

--- src/modules/auth/components/AuthForm.jsx
+++ src/modules/auth/components/AuthForm.jsx
@@ -1,7 +1,12 @@
 import AuthProvider from '@/modules/auth/provider';
+import { Button } from '@/modules/ui/button';
+import { Input } from '@/modules/ui/input';
+import { Label } from '@/modules/ui/label';
 import { useState } from 'react';
 const AuthForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const authProvider = new AuthProvider();
+
   const [error, setError] = useState(null);
   const login = async (e) => {
     e.preventDefault();
@@ -10,14 +15,29 @@ const AuthForm = () => {
     const formData = new FormData(form);
     setError(null);
     const { error } = await authProvider.login(formData.get('email'));
+    setIsLoading(false);
     setError(error);
   };
 
   return (
     <form method='post' onSubmit={login}>
-      <input type='email' placeholder='Email' name='email' />
-      <button type='submit'>Login</button>
-      {error && <span className='red'>{error.message}</span>}
+      <div className='grid gap-2'>
+        <div className='grid gap-1'>
+          <Label className='sr-only' htmlFor='email'>
+            Email
+          </Label>
+          <Input
+            id='email'
+            placeholder='name@example.com'
+            type='email'
+            autoCapitalize='none'
+            autoComplete='email'
+            autoCorrect='off'
+            disabled={isLoading}
+          />
+        </div>
+        <Button disabled={isLoading}>Log in with Email</Button>
+      </div>
     </form>
   );
 };
